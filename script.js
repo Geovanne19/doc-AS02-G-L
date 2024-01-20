9e15f3d56f4d04b80cda997f71bf0d64b9c4fd88
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2024-01-19 23:16:52 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function ativar_funcao (elemento) {
     var seta = elemento.querySelector('.seta')
     var oculto = elemento.querySelector('.oculto');
-    var cabecalho_funcao = elemento.querySelector('cabecalho-funcao')
+    var cabecalho_funcao = elemento.querySelector('.cabecalho-funcao')
 
     oculto.classList.toggle('exposto');
     seta.classList.toggle('exposto');
     cabecalho_funcao.classList.toggle('exposto')
-    
 }
 
 function run(elemento) {
+    var exemplo = elemento.querySelector('.exemplo')
+    console.log(exemplo)
     
+    exemplo.classList.toggle('exposto')
 }
